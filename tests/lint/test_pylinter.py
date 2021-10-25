f1bf9de51a596d5fa25a2f0b7f36050b9e68d209
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-10-25 21:41:01 +0200

    Add a regression tests for https://github.com/PyCQA/astroid/pull/1207

--- tests/lint/test_pylinter.py
+++ tests/lint/test_pylinter.py
@@ -3,9 +3,10 @@ from typing import Any
 from unittest.mock import patch
 
 from _pytest.capture import CaptureFixture
-from astroid import AstroidBuildingError
+from astroid import AstroidBuildingError, extract_node, nodes
 from py._path.local import LocalPath  # type: ignore
 
+from pylint.checkers.utils import safe_infer
 from pylint.lint.pylinter import PyLinter
 from pylint.utils import FileState
 
@@ -17,6 +18,20 @@ else:
 
 def raise_exception(*args: Any, **kwargs: Any) -> NoReturn:
     raise AstroidBuildingError(modname="spam")
+
+
+def test_safe_infer_on_bz2_compress_return():
+    """Regression tests for https://github.com/PyCQA/astroid/pull/1207"""
+    src_node = extract_node('import bz2\nunused = bz2.compress(b"")')
+    assignment = next(src_node.nodes_of_class(nodes.Assign))
+    inferred = safe_infer(assignment.value.func)
+    assert isinstance(inferred, nodes.FunctionDef), str(inferred)
+    try:
+        value = next(inferred.nodes_of_class(nodes.Return))
+        assert isinstance(value, nodes.Return)
+        assert value.parent.name == "compress"
+    except StopIteration:
+        assert False, f"No return found in body of:\n{str(inferred)}."
 
 
 @patch.object(FileState, "iter_spurious_suppression_messages", raise_exception)
